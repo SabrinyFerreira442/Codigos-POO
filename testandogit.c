631f8f5dcb13e57f15b2d16d7c47f75700850c04
SabrinyFerreira442 <sabrinyferreira442@gmail.com>
2024-09-03 17:10:07 -0300

    atualizada..

---
+++ testandogit.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("\nMeu primeiro contato com o git.");
+return 0;
+}
---
+++ testandoogit.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("Minha primeira experiência no GitHub!");
+return 0;
+}
